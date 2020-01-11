a39ebc9b834f763fe39385c68361d33a4464386d
Your Name <you@example.com>
2020-01-11 09:26:09 +0100

    smart Contract

--- JS/index.js
+++ JS/index.js
@@ -1,54 +1,55 @@
 const contractSource = `
 payable contract ParkingLot =   
-type i = int
-type s = string
-type a = address
+    type i = int
+    type s = string
+    type a = address
 
-record car = {
-    id : i,
-    owner : a,
-    nameOfCar	  : s,
-    nameOfOwner : s,
-    lisencePlate	  : s,
-    entryDate : i,
-    exitDate : i
-     }
-
-record state = 
-    { cars : map(i, car),
-    totalCars : i}
-
-entrypoint init() = {
-    cars = {},
-    totalCars = 0 }
-
-entrypoint getCar(index : i) : car = 
-    switch(Map.lookup(index, state.cars))
-        None  => abort("There is no Cars with that ID.")
-        Some(x) => x  
-
-stateful entrypoint addPlayer(nameOfCar' : s,nameOfOwner' : s, lisencePlate' : s) = 
+    record car = {
+        id : i,
+        owner : a,
+        nameOfCar	  : s,
+        nameOfOwner : s,
+        lisencePlate	  : s,
+        entryDate : i,
+        exitDate : i
+         }
    
-    let index = getTotalCars() + 1
-    let car = {id= index,  owner  = Call.caller, nameOfCar = nameOfCar', nameOfOwner = nameOfOwner',lisencePlate =  lisencePlate', entryDate = Chain.timestamp, exitDate = 0  }
-    put(state {cars[index] = car, totalCars = index})
+    record state = 
+        { cars : map(i, car),
+        totalCars : i}
+ 
+    entrypoint init() = {
+        cars = {},
+        totalCars = 0 }
 
-stateful entrypoint checkOut(index : i) = 
-    Chain.spend(Contract.address, 100000)
-    let carToCheckOut = getCar(index)
-    let updateDate = state.cars[index].entryDate
-    put(state{cars[index].exitDate = updateDate + Chain.block_height  })
+    entrypoint getCar(index : i) : car = 
+        switch(Map.lookup(index, state.cars))
+            None  => abort("There is no Cars with that ID.")
+            Some(x) => x  
 
-entrypoint getTotalCars() : i = 
-    state.totalCars
+    stateful entrypoint addPlayer(nameOfCar' : s,nameOfOwner' : s, lisencePlate' : s) = 
+       
+        let index = getTotalCars() + 1
+        let car = {id= index,  owner  = Call.caller, nameOfCar = nameOfCar', nameOfOwner = nameOfOwner',lisencePlate =  lisencePlate', entryDate = Chain.timestamp, exitDate = 0  }
+        put(state {cars[index] = car, totalCars = index})
 
-    
-// payable stateful entrypoint play(index : i, prize : i) =
-//     let detail = getPlayer(index)
-//     Chain.spend(detail.owner, prize)
-//     let updatedPrize = detail.amountWon + prize
-//     let updatedDetails = state.players{ [index].amountWon = updatedPrize }
-//     put(state{ players = updatedDetails })
+    stateful entrypoint checkOut(index : i) = 
+        Chain.spend(Contract.address, 100000)
+        let carToCheckOut = getCar(index)
+        let updateDate = state.cars[index].entryDate
+        put(state{cars[index].exitDate = updateDate + Chain.block_height  })
+
+    entrypoint getTotalCars() : i = 
+        state.totalCars
+
+        
+    // payable stateful entrypoint play(index : i, prize : i) =
+    //     let detail = getPlayer(index)
+    //     Chain.spend(detail.owner, prize)
+    //     let updatedPrize = detail.amountWon + prize
+    //     let updatedDetails = state.players{ [index].amountWon = updatedPrize }
+    //     put(state{ players = updatedDetails })
+
 
 `;
 const contractAddress ='ct_8b3NePSt93LaBWWuRmNQLwMzxwp3NS9afoMuf89rkH9LrhPEm';
